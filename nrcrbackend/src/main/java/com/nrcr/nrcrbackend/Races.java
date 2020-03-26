b2679013f620a48e7514fd9b7f7b80627d6aa72c
fullofallenisms <allengregory167@hotmail.com>
2020-03-26 19:21:29 +0000

    Working Solution for Races Collection.
    Includes two endpoints:
    - Retrieve All Race Details for All Racers
    - Retrieve All Race Details for Specified Racer
    
    Signed-off-by: fullofallenisms <allengregory167@hotmail.com>

--- nrcrbackend/src/main/java/com/nrcr/nrcrbackend/Races.java
+++ nrcrbackend/src/main/java/com/nrcr/nrcrbackend/Races.java
@@ -1,36 +1,47 @@
 package com.nrcr.nrcrbackend;
 
-import com.fasterxml.jackson.databind.JsonSerializable;
 import org.springframework.data.annotation.Id;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDate;
 
-@Document(collection = "A_Collection")
+@Document(collection = "test")
 public class Races {
 
     @Id
-    public ObjectId _id;
+    public String _id;
     @Field("Driver Name")
     public String driverName;
     @Field("Race Date")
     public LocalDate raceDate;
+    @Field("Qual Result Laps")
+    public String qualifyingResultLap;
+    @Field("Qual Result Time")
+    public String qualifyingResultTime;
+    @Field("Race Results Time")
+    public String raceResultTime;
+    @Field("Round or Leg")
+    public String roundOrLeg;
+    @Field("Race Lap Time")
+    public String raceLapTime;
+    @Field("Race Lap Number")
+    public String raceLapNumber;
 
-    public Races() {}
+    public Races() {
+    }
 
-    public Races(ObjectId _id, String driverName){
+    public Races(String _id, String driverName, LocalDate raceDate) {
         this._id = _id;
         this.driverName = driverName;
         this.raceDate = raceDate;
     }
 
-    public ObjectId get_id() {
+    public String get_id() {
         return _id;
     }
 
-    public void set_id(ObjectId _id) {
+    public void set_id(String _id) {
         this._id = _id;
     }
 
@@ -48,5 +59,53 @@ public class Races {
 
     public void setRaceDate(LocalDate raceDate) {
         this.raceDate = raceDate;
+    }
+
+    public String getQualifyingResultLap() {
+        return qualifyingResultLap;
+    }
+
+    public void setQualifyingResultLap(String qualifyingResultLap) {
+        this.qualifyingResultLap = qualifyingResultLap;
+    }
+
+    public String getQualifyingResultTime() {
+        return qualifyingResultTime;
+    }
+
+    public void setQualifyingResultTime(String qualifyingResultTime) {
+        this.qualifyingResultTime = qualifyingResultTime;
+    }
+
+    public String getRaceResultTime() {
+        return raceResultTime;
+    }
+
+    public void setRaceResultTime(String raceResultTime) {
+        this.raceResultTime = raceResultTime;
+    }
+
+    public String getRoundOrLeg() {
+        return roundOrLeg;
+    }
+
+    public void setRoundOrLeg(String roundOrLeg) {
+        this.roundOrLeg = roundOrLeg;
+    }
+
+    public String getRaceLapTime() {
+        return raceLapTime;
+    }
+
+    public void setRaceLapTime(String raceLapTime) {
+        this.raceLapTime = raceLapTime;
+    }
+
+    public String getRaceLapNumber() {
+        return raceLapNumber;
+    }
+
+    public void setRaceLapNumber(String raceLapNumber) {
+        this.raceLapNumber = raceLapNumber;
     }
 }
