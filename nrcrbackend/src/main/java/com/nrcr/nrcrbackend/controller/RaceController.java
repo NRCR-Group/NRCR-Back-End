ce701184808a46cc72ba70905eeeede4fa387a13
Sedgwick <adam.sedgwick@accenture.com>
2020-06-10 21:39:31 +0100

    Updated Pom XML File and updated the getRaceByDate endpoint

--- nrcrbackend/src/main/java/com/nrcr/nrcrbackend/controller/RaceController.java
+++ nrcrbackend/src/main/java/com/nrcr/nrcrbackend/controller/RaceController.java
@@ -3,8 +3,12 @@ package com.nrcr.nrcrbackend.controller;
 import com.nrcr.nrcrbackend.entity.RaceEntity;
 import com.nrcr.nrcrbackend.repository.RaceRepository;
 import org.bson.types.ObjectId;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,11 +32,15 @@ public class RaceController {
 //        return repository.findByDriverName(driverName);
 //    }
 //
-//    @RequestMapping(value = "/RaceDate", method = RequestMethod.GET)
-//    public List<Races> getRaceByDate(@RequestParam("raceDate")
-//                                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate raceDate){
-//        return repository.findByRaceDate(raceDate);
-//    }
+
+    // Example usage: http://localhost:8080/race-date/2018-04-23T00:00:00.000+00:00 or http://localhost:8080/race-date/2018-04-23
+    @RequestMapping(value = "/race-date/{raceDate}", method = RequestMethod.GET)
+    public RaceEntity getRaceByDate(@PathVariable("raceDate")
+                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date raceDate){
+        return repository.findByRaceDate(raceDate);
+    }
+
+
 //
 //    @RequestMapping(value = "/Season", method = RequestMethod.GET)
 //    public List<Races> getRaceBySeason(@RequestParam("season") String season){
