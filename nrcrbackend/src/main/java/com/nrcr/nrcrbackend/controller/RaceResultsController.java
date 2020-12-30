13d04490ed4fc92d4f897685157ece1269b204d5
Tesneem <zohaib.tesneem@accenture.com>
2020-12-30 01:28:54 +0000

    Removed additional '/' for Controller Methods to fix an error.

--- nrcrbackend/src/main/java/com/nrcr/nrcrbackend/controller/RaceResultsController.java
+++ nrcrbackend/src/main/java/com/nrcr/nrcrbackend/controller/RaceResultsController.java
@@ -20,30 +20,30 @@ public class RaceResultsController {
         this.resultsService = resultsService;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public List<RaceResultsEntity> getAllRaces() {
         return resultsService.getAllRaces();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public RaceResultsEntity getRaceById(@PathVariable("id") ObjectId id) {
         return resultsService.getRaceById(id);
     }
 
     // Example usage: http://localhost:8080/race-date/2018-04-23T00:00:00.000+00:00 or http://localhost:8080/race-date/2018-04-23
     @GetMapping
-    @RequestMapping(value = "/race-date/{raceDate}", method = RequestMethod.GET)
+    @RequestMapping(value = "race-date/{raceDate}", method = RequestMethod.GET)
     public RaceResultsEntity getRaceByDate(@PathVariable("raceDate")
                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date raceDate){
         return resultsService.getRaceByDate(raceDate);
     }
 
-    @PostMapping("/create-race-entity/")
+    @PostMapping("create-race-entity/")
     public RaceResultsEntity newRaceEntity(@RequestBody RaceResultsEntity newRaceEntity) {
         return resultsService.postNewRace(newRaceEntity);
     }
 
-    @DeleteMapping("/delete-race-entity/{id}")
+    @DeleteMapping("delete-race-entity/{id}")
     void deleteRaceEntity(@PathVariable ObjectId id) {
         String idString = id.toString();
         resultsService.deleteRace(idString);
